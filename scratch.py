f96e27b7b0c3d9ab520cafb6651ba1cf5ecf1055
samirmaassarani <samaassarani@gmail.com>
2025-06-18 22:21:55 +0100

    setting the starting skeleton

--- scratch.py
+++ scratch.py
@@ -6,16 +6,30 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/input', methods=['GET', 'POST'])
-def get_input():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     if request.method == 'POST':
-        seq = request.form['sequence']
-        return redirect(url_for('print_sequence', seq=seq))
-    return render_template('input.html')
+        seq = request.form['sequence'].upper()  # Get and uppercase the sequence
+        complement = get_complement(seq)  # Generate complement
+        return render_template('result.html',
+                            sequence=seq,
+                            complement=complement)
+    return render_template('index.html')
 
 @app.route('/display/<seq>')
 def print_sequence(seq):
     return render_template('display.html', sequence=seq)
+
+def check_seq():
+    seq= get_input
+    seq= seq.upper()
+    return seq
+
+def get_complement():
+    seq=get_input
+    complement_dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    return ''.join(complement_dict[base] for base in seq)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
